695c18b4a6cfe10eaeb7edc3b1391f37eee90ee3
Stefan Helms <mail@estefan.de>
2022-07-29 12:13:50 +0200

    searchfield finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import StyledButton from "./components/StyledButton";
+import StyledInput from "./components/StyledInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      return (
+          <div className="App">
+                <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo"/>
+                      <p>
+                            What do you want to buy?
+                      </p>
+                      <form
+                          className="inputBox"
+                          aria-labelledby="user"
+                          onSubmit={(event) => {
+                                //handleSubmit(event);
+                          }}
+                      >
+                            <StyledInput
+                                /*onFocus={(event) => {
+                                      clearOnFocus(event);
+                                }}
+                                onBlur={(event) => {
+                                      insertOnBlur(event);
+                                }}
+                                 */
+                                id="textInput"
+                                type="text"
+                                data-js="searchInput"
+                                className="searchField"
+                                placeholder="Search..."
+                                //value={inputValue}
+                                //onChange={handleInputValue}
+                            />
+                      </form>
+                </header>
+          </div>
+      );
 }
 
 export default App;
